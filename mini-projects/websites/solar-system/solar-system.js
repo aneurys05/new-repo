bd0f378a8af36082a44c638b3e910cd7e216471b
Aneurys Anglero <aneurys2090@gmail.com>
2026-03-28 17:00:53 -0400

    new web project

---
+++ mini-projects/websites/solar-system/solar-system.js
@@ -0,0 +1,15 @@
+
+
+
+const planets = [{name: "Mercury", picture: "images/fjbeeRiPRQjQNhizwy7cWX-1024-80.jpg"}, 
+                 {name: "Venus", picture: "images/venus-mariner-10-pia23791-fig2.jpg"}, 
+                 {name: "Earth", picture: "images/Meteosat-12-fci-march-equinox-2025-noon.jpg"}, 
+                 {name: "Mars", picture: "images/C454F5A6-536E-4C9F-AA6AF354BB85A85B_source.jpg"}, 
+                 {name: "Jupiter", picture: "images/Jupiter_OPAL_2024.png"}, 
+                 {name: "Jupiter", picture: "images/Jupiter_OPAL_2024.png"}, 
+                 {name: "Saturn", picture: "images/saturn.png"}, 
+                 {name: "Uranus", picture: "images/71aUHserTCL.jpg"},
+                 {name: "Neptune", picture: "images/Neptune_Full.jpg"}];
+
+
+let planetPic = document.getElementById("planet-pic");
