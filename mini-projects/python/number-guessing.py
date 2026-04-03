e4fbb523e77d7385c71c52d1e865bf7e511f33e2
Aneurys Anglero <aneurys2090@gmail.com>
2026-04-03 17:22:05 -0400

    Updated

--- mini-projects/python/number-guessing.py
+++ mini-projects/python/number-guessing.py
@@ -10,7 +10,7 @@ while True:
               user_guess = int(input("Enter a number between 1 and 100 \n"))
               if user_guess < 0 or user_guess > 100:
                  print("Number must be between 1 and 100 \n")
-                 continue
+                 
               else:
                   
                    if user_guess == random_number:
@@ -19,14 +19,14 @@ while True:
                       break
                    elif user_guess > random_number:
                       print("Too high, try again.")
-                      continue
+                      
                    elif user_guess < random_number:
                        print("Too low, try again.")
-                       continue
+                       
                    
           except ValueError:
                 print("Please make sure to enter  a number \n")
-                continue
+          
 
               
          
