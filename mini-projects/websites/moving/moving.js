cf34dce1c0bef3904b1acec9e2f6caf286fec992
Aneurys Anglero <aneurys2090@gmail.com>
2026-04-07 17:22:27 -0400

    finished

--- mini-projects/websites/moving/moving.js
+++ mini-projects/websites/moving/moving.js
@@ -1,8 +1,10 @@
-
-
-
 let container = document.getElementById("container");
+let up = 120;
+let left = 80;
 let speed = 5;
+let selection = document.getElementById("speed-selection");
+let button = document.querySelector("button");
+console.log(button);
 
 
 
@@ -11,7 +13,45 @@ document.body.addEventListener("keydown", handleEvent)
 
 function handleEvent(event) {
 
-console.log(event.key)
+switch (event.key){
+
+case "ArrowDown":
+      up += speed;
+      container.style.top = `${up}px`;
+      break;
+
+case "ArrowUp":
+      up -= speed;
+      container.style.top = `${up}px`;
+      break;
+
+case "ArrowRight":
+      left += speed;
+      container.style.left = `${left}px`;
+      break;
+
+case "ArrowLeft":
+      left -= speed;
+      container.style.left = `${left}px`;
+      break;
+
+}
 
 
 }
+
+button.addEventListener("click", ()=> {
+
+switch (selection.value){
+
+case "Fast":
+      speed = 10;
+      break;
+case "Slow":
+      speed = 1;
+      break;
+default:
+       speed = 5;
+
+}
+})
