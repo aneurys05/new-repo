3ebb9c0c6f503d37b6692c017640f17dbd9c3a5d
Aneurys Anglero <aneurys2090@gmail.com>
2026-04-11 16:40:32 -0400

    update

--- mini-projects/python/linked-list.py
+++ mini-projects/python/linked-list.py
@@ -9,8 +9,27 @@ class Nodes:
 
       def __str__(self):
           return self.value
+      
+      @classmethod
+      def display_nodes(cls):
+          node = node1
+          while node:
+                print(node.value)
+                node = node.next
 
+      @classmethod
+      def contains_node(cls, element):
+          node = node1
+          while node:
+                if node.value == element.upper():
+                   return f"Linked list contains {element}"
+                else:
+                     node = node.next
+          
+          return f"Linked list does not contain {element}"
+        
 
+        
 node1 = Nodes("A")
 node2 = Nodes("B")
 node3 = Nodes("C")
