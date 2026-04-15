acf54968fd2375d347aea6e9213f50611e87e51d
Aneurys Anglero <aneurys2090@gmail.com>
2026-04-15 17:31:22 -0400

    Added some functionality

--- mini-projects/python/students.py
+++ mini-projects/python/students.py
@@ -10,33 +10,47 @@ marks = {
 
 }
 
+
+def restart(iterable):
+    for item in iterable:
+        iterable[item] = 0
+
 print("-----Student registry-----")
 
-name = input("Please enter the students name \n")
+while True:
+            name = input("Please enter the students name \n")
 
 
-if name:
-        #Get marks 
-        for mark in marks:
+            if name:
+                    #Get marks 
+                    for mark in marks:
+                        
+                        while True:           
+                            try:
+                                marks[mark] = int(input(f"Please enter the score for {mark} \n"))
+                                if marks.get(mark) < 0 or marks.get(mark) > 100:
+                                    print("Number must be between 0 and 100 \n")
+                                    continue
+                                else:
+                                    break
+
+                            except ValueError:
+                                    print("Please make sure to enter a number \n")
+
+                    os.mkdir(f"{name}")
+                    with open(f"{name}/{name}-marks.txt", "x") as file:
+                        file.write(f"{name} marks! \n")
+                        file.write("\n")
+                        for mark in marks:
+                            file.write(f"{mark}: {marks.get(mark)} \n")          
             
-            while True:           
-                  try:
-                      marks[mark] = int(input(f"Please enter the score for {mark} \n"))
-                      if marks.get(mark) < 0 or marks.get(mark) > 100:
-                        print("Number must be between 0 and 100 \n")
-                        continue
-                      else:
-                          break
-
-                  except ValueError:
-                        print("Please make sure to enter a number \n")
-
-        os.mkdir(f"{name}")
-        with open(f"{name}/{name}-marks.txt", "x") as file:
-             file.write(f"{name} marks! \n")
-             file.write("\n")
-             for mark in marks:
-                 file.write(f"{mark}: {marks.get(mark)} \n")          
+            decision = input("Do you wish to register another student?: Y/N").lower()
+            if decision == "y":
+               restart(marks)
+               continue
+            else:
+                break
+               
 
 
             
