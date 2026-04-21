2eaef4577312319a291a487804a80f048cb758e0
Aneurys Anglero <aneurys2090@gmail.com>
2026-04-21 07:27:59 -0400

    updated

--- mini-projects/python/rock-paper-scissors.py
+++ mini-projects/python/rock-paper-scissors.py
@@ -5,6 +5,20 @@ options = ["rock", "paper", "scissors"]
 
 machine = random.choice(options)
 
+def show_result(r):
+    match r:
+          case "l":
+               print("You lost")
+               print(f"Your choice: {user}")
+               print(f"Computer's choice: {machine}")
+
+          case "w":
+               print("You won")
+               print(f"Your choice: {user}")
+               print(f"Computer's choice: {machine}")
+
+          case "e":
+               print("It's a tie")
 
 while True: 
       user = input("Please choose one: rock, paper or scissors \n").lower()
@@ -13,3 +27,13 @@ while True:
       else:
            break
       
+if user == machine:
+   show_result("e")
+elif user == "rock" and machine == "scissors":
+     show_result("w")
+elif user == "paper" and machine == "rock":
+     show_result("w")
+elif user == "scissors" and machine == "paper":
+     show_result("w")
+else:
+     show_result("l")
